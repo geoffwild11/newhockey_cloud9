1a25468fdbb39a192b12ac54f43e0822f1157b1a
Geoff Wild <geoffwild11@gmail.com>
2015-04-23 17:56:15 +0000

    Finished making changes to static pages controller

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,11 +4,25 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Home | NEWHockey"
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select 'title', "Help | NEWHockey"
+  end
+  
+  test "should get about" do
+    get :about
+    assert_response :success
+    assert_select 'title', "About | NEWHockey"
+  end
+  
+  test "should get teams" do
+    get :teams
+    assert_response :success
+    assert_select 'title', "Teams | NEWHockey"
   end
 
 end
