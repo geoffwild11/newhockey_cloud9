0a76d262da594b366a5ac57ab77b2ed854caab3d
Geoff Wild <geoffwild11@gmail.com>
2015-04-24 03:13:37 +0000

    Added some smarter stuff to helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  
+  # Return full title on page-by-page basis
+  def full_title(page_title = '')
+    base_title = "NEWHockey"
+    
+    if page_title.empty?
+      base_title
+    else
+      "#{page_title} | #{base_title}"
+    end
+  end
 end
