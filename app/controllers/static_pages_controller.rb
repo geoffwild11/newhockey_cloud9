02f204711532d8008a3febb684efdd6eedaef38c
Geoff Wild <geoffwild11@gmail.com>
2015-04-25 02:49:18 +0000

    Added contacts page and integration test

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -10,4 +10,7 @@ class StaticPagesController < ApplicationController
   
   def teams
   end
+  
+  def contact
+  end
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -29,5 +29,11 @@ class StaticPagesControllerTest < ActionController::TestCase
     assert_response :success
     assert_select 'title', "Teams | NEWHockey"
   end
+  
+  test "should get contact" do
+    get :contact
+    assert_response :success
+    assert_select 'title', "Contact | NEWHockey"
+  end
 
 end
