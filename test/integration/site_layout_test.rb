d248f9b7cd2fd6da74a82fd56707078bf6a072ba
Geoff Wild <geoffwild11@gmail.com>
2015-04-27 03:47:33 +0000

    Created Users table

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -1,10 +1,7 @@
 require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-  
+
   test "layout links" do
     get root_path
     assert_template 'static_pages/home'
@@ -12,6 +9,5 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", help_path
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
-    assert_select "a[href=?]", signup_path
   end
 end
